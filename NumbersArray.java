0f1e1f891886e2a29c6425d88e64965419443d4c
jlroch2024 <jl.roch.2024@smu.edu.sg>
2026-09-02 00:28:55 +0800

    Q1

--- NumbersArray.java
+++ NumbersArray.java
@@ -19,7 +19,42 @@ public class NumbersArray {
         System.out.println("Unique numbers : " + Arrays.toString(findUnique(input))); 
     }
 
-    // Write your methods here
+    public static int findMax(Integer[] input) {
+        int max = 0;
+        for (int i = 0; i < input.length; i++) {
+            if (input[i] > max) {
+                max = input[i];
+            }
+        }
+        
+        return max;
+    }
+
+
+    public static Integer[] findDuplicates(Integer[] input) {
+        Set<Integer> seenBefore = new HashSet<>();
+        Set<Integer> duplicateNumbers = new HashSet<>();
+        for (Integer num : input) {
+            if (!seenBefore.add(num)) {
+                duplicateNumbers.add(num);
+            }
+        }
+        return duplicateNumbers.toArray(new Integer[0]);
+    }
     
+    public static Integer[] findUnique(Integer[] input) {
+        Set<Integer> duplicateNumbers = new HashSet<>();
+        Set<Integer> uniqueNumbers = new HashSet<>();
+        for (Integer num : input) {
+            if (!duplicateNumbers.contains(num)) {
+                if (!uniqueNumbers.add(num)) {
+                uniqueNumbers.remove(num);
+                duplicateNumbers.add(num);
+            }
+
+            
+        }
+        return uniqueNumbers.toArray(new Integer[0]);
+    }
 }
 
